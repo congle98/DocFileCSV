b806927cc03acf1a7a53de33bd19deec10269a61
congle98 <79907265+congle98@users.noreply.github.com>
2021-04-26 09:26:19 +0700

    addfile

---
+++ src/demo.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class demo {
+    public static void main(String[] args) {
+//        BufferedReader br = null;
+//        try {
+//            String line;
+//            br = new BufferedReader(new FileReader("countries.csv"));
+//
+//            while ((line = br.readLine()) != null) {
+//                List<String> data = parseCsvLine(line);
+//                printCountry(data);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (br != null)
+//                    br.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+
+
+
+//        String str = "anh cong, anh a, anh b, anh c";
+//        String [] data = str.split(",");
+//        for (int i = 0; i < data.length; i++) {
+//            System.out.println(data[i]);
+//        }
+
+//    }
+//    public static List<String> parseCsvLine(String csvLine) {
+//        List<String> result = new ArrayList<>();
+//        if (csvLine != null) {
+//            String[] splitData = csvLine.split(",");
+//            for (int i = 0; i < splitData.length; i++) {
+//                result.add(splitData[i]);
+//            }
+//        }
+//        return result;
+//    }
+//
+//    private static void printCountry(List<String> country) {
+//        System.out.println(
+//                "Country [id= "
+//                        + country.get(0)
+//                        + ", code= " + country.get(1)
+//                        + " , name=" + country.get(2)
+//                        + "]");
+//    }
+//
+//}
